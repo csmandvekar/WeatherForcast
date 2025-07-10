d4b921bc172e788eef5d9233a844b97d5838cb58
csmandvekar <csmandvekar@gmail.com>
2025-07-10 17:06:43 +0530

    deployment env

--- app/config.py
+++ app/config.py
@@ -1,7 +1,9 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///weather.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///weather.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    OPENWEATHERMAP_API_KEY = open('apikey.txt', 'r').read().strip()
+    OPENWEATHERMAP_API_KEY = os.environ.get('OPENWEATHERMAP_API_KEY')
     WEATHER_UPDATE_INTERVAL = 300  # in seconds (5 minutes)
     ALERT_THRESHOLD_TEMP = 35  # in Celsius
     ALERT_CONSECUTIVE_COUNT = 2
